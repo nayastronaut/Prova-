6bd3dbb1b383e6d4c509dd29dcd4a9d4a9c6c7c2
nayastronaut <naylabrizola@outlook.com>
2024-10-16 16:48:47 -0300

    atualizacao no banco

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async() => 
 mongoose
-.connect("mongodb://localhost:27017/banco")
+.connect(`${process.env.URI}`)
 .then(() => console.log("Conectado ao MongoDB"))
 .catch((erro) => console.error("Erro ao conectar ao MongoDB:", erro));
 
